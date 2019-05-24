7beb5129a57544b5c1d90ffa1967e314600a3039
Caribou <valentinbdu79@gmail.com>
2019-05-24 10:03:02 +0200

    Connexion opérationnel

--- Oukilestkiki/Controllers/LoginController.cs
+++ Oukilestkiki/Controllers/LoginController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BO;
 
 namespace Oukilestkiki.Controllers
 {
     public class LoginController : Controller
     {
+        private Context db = new Context();
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Utilisateur utilisateur)
+        {
+            var userConnect = db.Utilisateurs.FirstOrDefault(user => user.Mail == utilisateur.Mail);
+            if (userConnect.Password == utilisateur.Password)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Login");
         }
     }
 }
