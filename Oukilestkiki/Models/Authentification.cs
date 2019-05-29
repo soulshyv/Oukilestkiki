bfddcf5b18cbdcbd1aa3efe47ad2d57ec3cc0903
soulshyv <francois.fasquel2017@campus-eni.fr>
2019-05-29 10:10:00 +0200

    vérif si connecté pour ajouter le user à l'animal de la recherche + corrections édit recherche photos

---
+++ Oukilestkiki/Models/Authentification.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using BO;
+
+namespace Oukilestkiki.Models
+{
+    public static class Authentification
+    {
+        public static bool EstConnecte()
+        {
+            var user = (Utilisateur)HttpContext.Current.Session["Utilisateur"];
+            if (user != null)
+            {
+                if (user.Role.Libelle.Equals("Admin"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static Utilisateur GetSessionUtilisateur()
+        {
+            var user = HttpContext.Current.Session["Utilisateur"];
+            if (user != null)
+            {
+                return (Utilisateur) user;
+            }
+
+            return null;
+        }
+    }
+}
